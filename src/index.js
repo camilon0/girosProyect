1a7fd4b045d842bb2572956a556e8ff372f4f1db
camilon0 <kmilo8433@gmail.com>
2022-09-29 10:47:36 -0500

    puerto

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const router = jsonServer.router(`${__dirname}/data/db.json`);
 const port = process.env.PORT || 3000;
 
 server.use(router);
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("JSON Server is running");
 });
